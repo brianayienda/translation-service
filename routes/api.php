593ff493ce5994540f7fc76fe3aa23d13fa6c74a
brianayienda <ayiendabrian37@gmail.com>
2025-12-17 00:11:13 +0300

    Test Passed

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TranslationController;
 
-
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('translations', TranslationController::class);
+    // Only the routes you actually use
+    Route::get('translations', [TranslationController::class, 'index']);
+    Route::post('translations', [TranslationController::class, 'store']);
     Route::get('translations/search', [TranslationController::class, 'search']);
     Route::get('translations/export', [TranslationController::class, 'export']);
+    Route::put('translations/{translation}', [TranslationController::class, 'update']);
+    Route::delete('translations/{translation}', [TranslationController::class, 'destroy']);
 });
